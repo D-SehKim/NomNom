132aebab5c18a8015549a55fe72c9484b56a79c0
ericyangchen <yericchenyang@gmail.com>
2025-10-29 11:22:00 -0400

    feat: user login rspec test

--- NomNom/spec/factories/users.rb
+++ NomNom/spec/factories/users.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "user#{n}@example.com" }
+    email { Faker::Internet.email }
     password { "password123" }
     password_confirmation { "password123" }
+    
+    trait :with_grocery_items do
+      after(:create) do |user|
+        create_list(:grocery_item, 3, user: user)
+      end
+    end
   end
 end
--- NomNom/spec/rails_helper.rb
+++ NomNom/spec/rails_helper.rb
@@ -9,20 +9,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # return unless Rails.env.test?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'simplecov'
-SimpleCov.start 'rails' do
-  add_filter '/spec/' # ignore test files from coverage
-end
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
-
-require 'webmock/rspec'
-WebMock.disable_net_connect!(allow_localhost: true)
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -36,7 +23,7 @@ WebMock.disable_net_connect!(allow_localhost: true)
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
 
 # Ensures that the test database schema matches the current schema file.
 # If there are pending migrations it will invoke `db:test:prepare` to
@@ -48,10 +35,6 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
-  # Allows using `create(:user)` instead of `FactoryBot.create(:user)`
-  config.include FactoryBot::Syntax::Methods         # so you can use `create(:user)`
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::IntegrationHelpers, type: :request
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
@@ -86,4 +69,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  # Include FactoryBot syntax methods
+  config.include FactoryBot::Syntax::Methods
+  
+  # Include Devise test helpers
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
